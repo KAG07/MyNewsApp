18baeda921cc1f9d2ff92cf2874398b774aa54b9
Kunal Agrawal <91177548+KAG07@users.noreply.github.com>
2022-03-11 15:51:04 +0530

    Update Constants.kt

--- app/src/main/java/com/example/mynews/ui/util/Constants.kt
+++ app/src/main/java/com/example/mynews/ui/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.mynews.ui.util
 
 class Constants {
     companion object {
-        const val api_Key = "745006a26c524e52aebd29d17b17893e"
+        const val api_Key = ""
         const val base_url = "https://newsapi.org"
         const val search_news_delay=500L
         const val QUERY_PAGE_CNT=20
